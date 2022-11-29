a7efeabcb6abf6cab7e1a10aa69d8c0cbc5aa44c
Richmond Kessie <kessierich.mag@gmail.com>
2022-11-29 11:53:17 +0000

    Singly linked list

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -prints the alphabet in lowercase
+ *followed by a new line except q and e
+ *Return: Always 0
+ */
+int main(void)
+{
+char la;
+for (la = 'a'; la <= 'z'; la++)
+{
+if (la != 'e' && la != 'q')
+{
+putchar(la);
+}
+}
+putchar('\n');
+return (0);
+}
